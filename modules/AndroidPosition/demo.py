61c97e90a3f78ed65f1d18656e910064e5a8019c
Fenil Mehta <fenilgmehta@gmail.com>
2019-06-06 21:21:05 +0530

    7 files reformatted and indentation changed to spaces The committed files are either Python 2 and 3 compatible or they are already written in Python 3. Following is the list - 1. mapmint-services/customize.py 2. mapmint-services/mapfile/mmSprites.py 3. mapmint-services/print/PaperMint.py 4. mapmint-services/service.py 5. mapmint-services/sqlite_module.py 6. mapmint-services/vector-tools-src/cgi-env/mmservice.py 7. modules/AndroidPosition/demo.py - indentation error fixed

--- modules/AndroidPosition/demo.py
+++ modules/AndroidPosition/demo.py
@@ -1,13 +1,15 @@
 import zoo
 import sys
 
-def defineLocation(conf,inputs,outputs):
-	conf["senv"]["cPosition"]=inputs["lonlat"]["value"]
-        outputs["Result"]["value"]="Done"
-        return zoo.SERVICE_SUCCEEDED
 
-def getLocation(conf,inputs,outputs):
-	import json
-	polux=conf["senv"]["cPosition"].split(",")
-	outputs["Result"]["value"]=json.dumps(polux)
-	return zoo.SERVICE_SUCCEEDED
+def defineLocation(conf, inputs, outputs):
+    conf["senv"]["cPosition"] = inputs["lonlat"]["value"]
+    outputs["Result"]["value"] = "Done"
+    return zoo.SERVICE_SUCCEEDED
+
+
+def getLocation(conf, inputs, outputs):
+    import json
+    polux = conf["senv"]["cPosition"].split(",")
+    outputs["Result"]["value"] = json.dumps(polux)
+    return zoo.SERVICE_SUCCEEDED
