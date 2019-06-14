70003616bc86b7669630cb2ad6d8034d36f41193
Fenil Mehta <fenilgmehta@gmail.com>
2019-06-14 15:18:12 +0530

    Three files reformatted and indentation changed to spaces Path to each file is as follows: 1. mapmint/mapmint-services/mm_access.py 2. mapmint/mapmint-services/mmowssp.py 3. mapmint/mapmint-services/mmsession.py

--- mapmint-services/mmsession.py
+++ mapmint-services/mmsession.py
@@ -1,36 +1,36 @@
 import zoo
 
+
 def save(conf):
     try:
         _save(conf)
-        fname="sess_"+conf["senv"]["MMID"]+".cfg"
-        if list(conf["main"].keys()).count("sessPath")>0:
-            fullName=conf["main"]["sessPath"]+"/"+fname
+        fname = "sess_" + conf["senv"]["MMID"] + ".cfg"
+        if list(conf["main"].keys()).count("sessPath") > 0:
+            fullName = conf["main"]["sessPath"] + "/" + fname
         else:
-            fullName=conf["main"]["tmpPath"]+"/"+fname
-        f=open(fullName,"w")
+            fullName = conf["main"]["tmpPath"] + "/" + fname
+        f = open(fullName, "w")
         f.write("[senv]\n")
         for j in conf["senv"]:
-            f.write(j+"="+conf["senv"][j]+"\n")
+            f.write(j + "=" + conf["senv"][j] + "\n")
         f.close()
         return zoo.SERVICE_SUCCEEDED
     except Exception as e:
-        conf["lenv"]["message"]="Error occured when trying to save session: "+str(e)
+        conf["lenv"]["message"] = "Error occured when trying to save session: " + str(e)
         return zoo.SERVICE_FAILED
+
 
 def _save(conf):
     try:
-        fname="sess_"+conf["senv"]["MMID"]+"_1.cfg"
-        if list(conf["main"].keys()).count("sessPath")>0:
-            fullName=conf["main"]["sessPath"]+"/"+fname
+        fname = "sess_" + conf["senv"]["MMID"] + "_1.cfg"
+        if list(conf["main"].keys()).count("sessPath") > 0:
+            fullName = conf["main"]["sessPath"] + "/" + fname
         else:
-            fullName=conf["main"]["tmpPath"]+"/"+fname
-        f=open(fullName,"w")
+            fullName = conf["main"]["tmpPath"] + "/" + fname
+        f = open(fullName, "w")
         f.write("[senv]\n")
         for j in conf["senv"]:
-            f.write(j+"="+conf["senv"][j]+"\n")
-        f.close()        
+            f.write(j + "=" + conf["senv"][j] + "\n")
+        f.close()
     except Exception as e:
-        conf["lenv"]["message"]="Error occured when trying to save session: "+str(e)
-        
-    
+        conf["lenv"]["message"] = "Error occured when trying to save session: " + str(e)
